2ddd5eb82205be134cc42fa12ed07d4059c95dd3
Mob <49191414+maframo@users.noreply.github.com>
2019-05-04 18:22:10 +0200

    Arreglat bug
    Quan premies enter per que aparegues el cano, el fighter feia l'naimacio puny esquerre

--- Street Fighter/Scripts/Fighter.gd
+++ Street Fighter/Scripts/Fighter.gd
@@ -50,7 +50,7 @@ func animate():
 		elif velocitat.x == 0 and is_on_floor():
 			$AnimatedSprite.animation = "quiet"
 	
-	if Input.is_action_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_pressed("ui_select") and is_on_floor():
 		$AnimatedSprite.animation = "pega_esq"
 		pegant = true
 	
